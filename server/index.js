257c532956973fc41fcd7cce48f0bd9c9b3c5f7d
Stepan Svechnikov <dumn2011@gmail.com>
2024-10-27 20:25:08 +0300

    feat: add env for server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 
-const port = 5000
+require('dotenv').config()
 
+const port = process.env.SERVER_PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('base')
