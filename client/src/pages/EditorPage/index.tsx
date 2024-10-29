22f506f63d726da7e5a703d7033d1295f07b46d5
Stepan Svechnikov <dumn2011@gmail.com>
2024-10-29 21:59:48 +0300

    feat: add exampled config for names and colors

--- client/src/pages/EditorPage/index.tsx
+++ client/src/pages/EditorPage/index.tsx
@@ -5,14 +5,16 @@ import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
 import {CollaborationPlugin} from '@lexical/react/LexicalCollaborationPlugin';
 import * as Y from 'yjs';
 // import {$initialEditorState} from './initialEditorState';
-import {WebsocketProvider} from 'y-websocket';
-import {useCallback} from "react";
+import {messageAwareness, WebsocketProvider} from 'y-websocket';
+import {useCallback, useEffect, useState} from "react";
 import {Provider} from '@lexical/yjs';
 import {WebrtcProvider} from 'y-webrtc';
 
 import s from './Editor.module.css'
 
 export const EditorPage = () => {
+  const [userProfile, setUserProfile] = useState(() => getRandomUserProfile());
+
 
   const initialConfig = {
     // NOTE: This is critical for collaboration plugin to set editor state to null. It
@@ -27,15 +29,33 @@ export const EditorPage = () => {
     theme: {},
   };
 
+  const providerName = 'webrtc';
+
   const providerFactory = useCallback(
     (id: string, yjsDocMap: Map<string, Y.Doc>) => {
-      const doc = getDocFromMap(id, yjsDocMap);
+      const provider =
+        providerName === 'webrtc'
+          ? createWebRTCProvider(id, yjsDocMap)
+          : createWebsocketProvider(id, yjsDocMap);
 
-      return new WebsocketProvider(`ws://${import.meta.env.APP_SERVER_HOST}:${import.meta.env.APP_SERVER_PORT}`, id, doc, {
-        connect: false,
+      provider.on('status', (event) => {
+        console.log("event: ", event);
       });
+
+
+      return provider
     }, [],
   );
+
+  useEffect(() => {
+    (function (postMessage) {
+      BroadcastChannel.prototype.postMessage = function (message) {
+        // debugger;
+        console.log("message: ", message)
+        postMessage.call(this, message);
+      };
+    }(BroadcastChannel.prototype.postMessage));
+  }, [])
 
   return (
     <main className={s.root}>
@@ -46,15 +66,15 @@ export const EditorPage = () => {
         />
         <CollaborationPlugin
           id="collab"
-          providerFactory={createWebRTCProvider}
+          providerFactory={providerFactory}
           // Optional initial editor state in case collaborative Y.Doc won't
           // have any existing data on server. Then it'll user this value to populate editor.
           // It accepts same type of values as LexicalComposer editorState
           // prop (json string, state object, or a function)
           // initialEditorState={$initialEditorState}
           shouldBootstrap={true}
-          username={"test"}
-          cursorColor={"red"}
+          username={userProfile.name}
+          cursorColor={userProfile.color}
         />
       </LexicalComposer>
     </main>
@@ -82,16 +102,7 @@ function createWebsocketProvider(
   id: string,
   yjsDocMap: Map<string, Y.Doc>,
 ): Provider {
-  let doc = yjsDocMap.get(id);
-
-  if (doc === undefined) {
-    doc = new Y.Doc();
-    yjsDocMap.set(id, doc);
-  } else {
-    doc.load();
-  }
-
-  console.log("doc: ", doc)
+  const doc = getDocFromMap(id, yjsDocMap);
 
   // @ts-expect-error
   return new WebsocketProvider(
@@ -100,7 +111,6 @@ function createWebsocketProvider(
     doc,
     {
       connect: false,
-
     },
   );
 }
@@ -118,8 +128,43 @@ const createWebRTCProvider = (id: string,
     peerOpts: {
       reconnectTimer: 100,
     },
-
+    // awareness: messageAwareness,
+    filterBcConns: true,
     signaling:
-      window.location.hostname === 'localhost' ? [`${WEBSOCKET_ENDPOINT}/collab`] : [`${WEBSOCKET_ENDPOINT}/collab`],
+      window.location.hostname === 'localhost' ? [`${WEBSOCKET_ENDPOINT}/collab`] : [],
   });
+}
+
+
+
+const entries: [string, string][] = [
+  ['Arabian', '#7d0000'],
+  ['Appaloosa', '#640000'],
+  ['Friesian', '#990000'],
+  ['Thoroughbred', '#bf0000'],
+  ['Warmblood', '#bf4000'],
+  ['Saddlebred', '#004000'],
+  ['Mustang', '#007f00'],
+  ['Trakehner', '#407f00'],
+  ['Quarter Horse', '#7f7f00'],
+  ['Clydesdale', '#000099'],
+  ['Paint', '#0000bf'],
+  ['Icelandic', '#0000ff'],
+  ['Andalusian', '#004040'],
+  ['Tennessee Walker', '#404040'],
+  ['Ukrainian Riding Horse', '#7f0040'],
+  ['Percheron', '#bf0040'],
+];
+
+export interface UserProfile {
+  name: string;
+  color: string;
+}
+
+export function getRandomUserProfile(): UserProfile {
+  const entry = entries[Math.floor(Math.random() * entries.length)];
+  return {
+    color: entry[1],
+    name: entry[0],
+  };
 }
